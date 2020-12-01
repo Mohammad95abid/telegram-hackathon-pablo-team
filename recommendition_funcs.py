c916bf6ee95e70f8fb0ad9c7157e5b8933816f75
qasim <qasim.habiballa@gmail.com>
2020-12-01 18:22:11 +0200

    ss

--- recommendition_funcs.py
+++ recommendition_funcs.py
@@ -1,24 +1,31 @@
 
 
-from paplo_db_api import get_review_by_user_id,get_all_users_id,is_user_exist,add_book_rating,create_user
+from paplo_db_api import get_review_by_user_id,get_all_users_id,add_book_rating,create_user,add_book,is_book_review_exist,update_review
 import random
+from  paplo_db_api import  is_user_exist as exist
 import goodreads_api_client as gr
 def is_user_exist(user_id):
-    return is_user_exist(user_id)
+    return exist(user_id)
 def get_description(book_title):
     client = gr.Client(developer_key='q5QJR1BpwdBHs7SLjH0mw')
     book = client.Book.title(book_title)
-    print(book)
+    return book['description']
 
 def rate_book(user_id,book_title,is_like,*args):
-    if is_user_exist(user_id):
-        add_book_rating(book_title,user_id,is_like)
-
-    else:
+    if not exist(user_id):
         first_name=args[0]
         last_name=args[1]
         create_user(user_id,first_name,last_name)
-        add_book_rating(book_title,user_id,is_like)
+
+    if not is_book_exist(book_title):
+        description=get_description(book_title)
+        add_book(book_title,description,"not found","not found","action")
+
+    update_review(book_title,user_id,is_like)
+
+
+
+
 def get_recommendation_author(user_id, book_title):
     client = gr.Client(developer_key='q5QJR1BpwdBHs7SLjH0mw')
     book = client.Book.title(book_title)
@@ -33,7 +40,7 @@ def get_review_by_booktitle(user_reviews,book_title):
             return review
     return None
 
-def get_book(user_id):
+def get_book1(user_id):
     user_reviews = get_review_by_user_id(user_id)
     all_user = get_all_users_id()
     max = 0
@@ -52,11 +59,10 @@ def get_book(user_id):
             if cur_review == None:
                 book_to_recommend.append(review['book_title'])
                 continue
-
-            if (cur_review['like_'] == True and review['like_'] == False) or (cur_review['like_'] == False and review['like_'] == True):
+            if (bool(cur_review['like_']) == True and bool(review['like_']) == False) or (bool(cur_review['like_']) == False and bool(review['like_']) == True):
                 similarity-=1
 
-            elif (cur_review['like_'] == True and review['like_'] == True) or (cur_review['like_'] == False and review['like_'] == False):
+            elif (bool(cur_review['like_']) == True and bool(review['like_']) == True) or (bool(cur_review['like_']) == False and bool(review['like_']) == False):
                 similarity+=1
 
         if max_similarty_user == None:
@@ -71,5 +77,5 @@ def get_book(user_id):
 
     return random.choice(new_book_to_recommend)
 
-
-get_description("The Last Wish (The Witcher, #0.5)")
+#print(get_book("esv12335"))
+#print(get_description("The Last Wish (The Witcher, #0.5)"))
